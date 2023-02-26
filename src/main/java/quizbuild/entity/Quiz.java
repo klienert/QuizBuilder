859c944b2ded1dbce739be29e815bafd6e02f81c
Keith <klienert@madisoncollege.edu>
2023-02-26 11:44:49 -0600

    added quiz and user classes

---
+++ src/main/java/quizbuild/entity/Quiz.java
@@ -0,0 +1,14 @@
+package quizbuild.entity;
+
+/**
+ * Quiz object
+ * @author keithlienert
+ */
+public class Quiz {
+    private int id;
+    private String title;
+    private String summary;
+    private int type;
+    private double score;
+
+}
--- src/main/java/quizbuild/entity/User.java
+++ src/main/java/quizbuild/entity/User.java
@@ -1,10 +1,21 @@
 package quizbuild.entity;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * public class object for User
+ * @author keithlienert
  */
 public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String email;
+    private Set<Quiz> quizzes = new HashSet<>();
 
 
 
