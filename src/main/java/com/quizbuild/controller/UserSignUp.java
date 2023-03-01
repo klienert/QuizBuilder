f6820ab38737c32dfdcf89713e863507a366ad89
Keith <klienert@madisoncollege.edu>
2023-03-01 17:26:34 -0600

    added userSearch and userSignUp classes, need to create the rest of the signup pages

--- src/main/java/com/quizbuild/controller/UserSignUp.java
+++ src/main/java/com/quizbuild/controller/UserSignUp.java
@@ -1,2 +1,44 @@
-package com.quizbuild.controller;public class UserSignUp {
+package com.quizbuild.controller;
+
+import com.quizbuild.entity.User;
+import com.quizbuild.persistence.GenericDao;
+import com.quizbuild.util.DaoFactory;
+import org.apache.logging.log4j.*;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet (
+        urlPatterns = {"/signUpUser"}
+)
+
+public class UserSignUp extends HttpServlet {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        User user = new User();
+        // set fields
+        user.setUserName(req.getParameter("userName"));
+        user.setEmail(req.getParameter("email"));
+        user.setFirstName(req.getParameter("firstName"));
+        user.setLastName(req.getParameter("lastName"));
+        user.setPassword(req.getParameter("password"));
+        logger.debug("Adding User: " + user);
+
+
+        // probably do some type of verification...
+        GenericDao dao = DaoFactory.createDao(User.class);
+        dao.insert(user);
+
+    }
+    RequestDispatcher dispatcher = req.getRequestDispatcher("/signUpConfirmation.jsp");
+        dispatcher.forward(req, resp);
 }
